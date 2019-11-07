372148910377bf45368eb2b1961b26adcf5704ae
Ankit Rawat <ankitrawat@Ankits-MacBook-Pro.local>
2019-11-07 12:30:58 +0530

    adding test create parking lot

--- src/test/java/com/gojek/Delegate/ParkingDelegateImplTest.java
+++ src/test/java/com/gojek/Delegate/ParkingDelegateImplTest.java
@@ -3,12 +3,22 @@ package com.gojek.Delegate;
 import com.gojek.Delegates.ParkingDelegateImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ParkingDelegateImplTest {
 
     ParkingDelegateImpl parkingDelegate;
 
+    Service.ParkingLotService parkingLotService;
 
+    @Test
+    void shouldCreateParkingLot()
+    {
+        parkingDelegate.createParkingLot(5);
+        verify(parkingLotService).createParkingLot(5);
+    }
 
 }
