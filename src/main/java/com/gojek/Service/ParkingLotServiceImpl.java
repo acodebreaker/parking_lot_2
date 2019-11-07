ff1ac8c5bb28ec410fe56de027b92aa3acdd2c58
Ankit Rawat <ankitrawat@Ankits-MacBook-Pro.local>
2019-11-07 14:46:22 +0530

    implementing interface to pass test

--- src/main/java/com/gojek/Service/ParkingLotServiceImpl.java
+++ src/main/java/com/gojek/Service/ParkingLotServiceImpl.java
@@ -1,12 +1,49 @@
 package com.gojek.Service;
 
+import com.gojek.Exceptions.NoSuchSlotException;
+import com.gojek.Exceptions.SlotNotAvailableException;
+import com.gojek.Model.ParkingSlot;
+import com.gojek.Model.Vehicle;
+
+import java.util.List;
+
 public class ParkingLotServiceImpl implements Service.ParkingLotService {
 
 
 
-
+    @Override
     public void createParkingLot(int totalSlots) {
 
+    }
+
+    @Override
+    public ParkingSlot parkVehicle(Vehicle vehicle) throws SlotNotAvailableException {
+        return null;
+    }
+
+    @Override
+    public void leaveParkingSlot(int parkingSlotNumber) throws NoSuchSlotException {
+
+    }
+
+    @Override
+    public List<ParkingSlot> getStatusOfParkingLot() {
+        return null;
+    }
+
+    @Override
+    public List<Integer> getSlotNumbersForCarsWithColor(String color) {
+        return null;
+    }
+
+    @Override
+    public int getSlotNumberForRegistrationNumber(String registrationNumber) {
+        return 0;
+    }
+
+    @Override
+    public List<String> getRegistrationNumbersForCarsWithColor(String color) {
+        return null;
     }
 
 }
