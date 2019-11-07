7688044ccaaf12913680507c7dfc8b257918ca31
Ankit Rawat <ankitrawat@Ankits-MacBook-Pro.local>
2019-11-07 14:58:16 +0530

    adding test for parking delegate

--- src/test/java/com/gojek/Delegate/ParkingDelegateImplTest.java
+++ src/test/java/com/gojek/Delegate/ParkingDelegateImplTest.java
@@ -1,6 +1,9 @@
 package com.gojek.Delegate;
 
 import com.gojek.Delegates.ParkingDelegateImpl;
+import com.gojek.Exceptions.NoSuchSlotException;
+import com.gojek.Exceptions.SlotNotAvailableException;
+import com.gojek.Model.Vehicle;
 import com.gojek.Service.ParkingLotServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,6 +27,48 @@ public class ParkingDelegateImplTest {
     {
         parkingDelegate.createParkingLot(5);
         verify(parkingLotService).createParkingLot(5);
+    }
+
+    @Test
+    public void testParkVehicle() throws SlotNotAvailableException {
+        Vehicle mockVehicle = new Vehicle();
+        parkingDelegate.parkVehicle(mockVehicle);
+        verify(parkingLotService).parkVehicle(mockVehicle);
+
+    }
+
+    @Test
+    public void testLeave() throws NoSuchSlotException {
+        parkingDelegate.leaveParkingSlot(1);
+        verify(parkingLotService).leaveParkingSlot(1);
+
+    }
+
+    @Test
+    public void testGetStatusOfParkingLot() {
+        parkingDelegate.getStatusOfParkingLot();
+        verify(parkingLotService).getStatusOfParkingLot();
+
+    }
+
+    @Test
+    public void testGetRegistrationNumbersForCarsWithColor() {
+        parkingDelegate.getRegistrationNumbersForCarsWithColor("red");
+        verify(parkingLotService).getRegistrationNumbersForCarsWithColor("red");
+    }
+
+    @Test
+    public void testGetSlotNumbersForCarsWithColor() {
+        parkingDelegate.getSlotNumbersForCarsWithColor("red");
+        verify(parkingLotService).getSlotNumbersForCarsWithColor("red");
+
+    }
+
+    @Test
+    public void testGetSlotNumberForRegistrationNumber() {
+        parkingDelegate.getSlotNumberForRegistrationNumber("MH 12 4567");
+        verify(parkingLotService).getSlotNumberForRegistrationNumber("MH 12 4567");
+
     }
 
 
