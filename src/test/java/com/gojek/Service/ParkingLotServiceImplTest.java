72e1198ecec5ee7bc42e86e29e56f3d6aea92a8e
Ankit Rawat <ankitrawat@Ankits-MacBook-Pro.local>
2019-11-07 15:16:18 +0530

    adding test class for service

---
+++ src/test/java/com/gojek/Service/ParkingLotServiceImplTest.java
@@ -0,0 +1,71 @@
+package com.gojek.Service;
+
+import com.gojek.Exceptions.NoSuchSlotException;
+import com.gojek.Exceptions.SlotNotAvailableException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ParkingLotServiceImplTest {
+
+    ParkingLotServiceImpl parkingLotService;
+
+    @Before
+    public void setUp() throws Exception {
+        parkingLotService = new ParkingLotServiceImpl();
+        parkingLotService.createParkingLot(5);
+    }
+
+
+    @Test
+    public void testCreateParkingLot() {
+
+
+
+
+    }
+
+    @Test
+    public void testParkVehicle() throws SlotNotAvailableException {
+
+
+
+    }
+
+    @Test
+    public void testLeaveSpot() throws SlotNotAvailableException, NoSuchSlotException {
+
+
+
+    }
+
+    @Test
+    public void testGetSlotNumberByColor() throws SlotNotAvailableException {
+
+
+    }
+
+    @Test
+    public void testGetSlotNumberForRegistrationNumber() throws SlotNotAvailableException {
+
+
+    }
+
+    @Test
+    public void testGetRegistrationNumbersForCarsWithColor() throws SlotNotAvailableException {
+
+
+    }
+
+
+
+
+
+
+}
