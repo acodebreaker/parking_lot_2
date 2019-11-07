ac24a30d09c8f7fac2ba64dd5d1cf372068b5bc7
Ankit Rawat <ankitrawat@Ankits-MacBook-Pro.local>
2019-11-08 02:16:19 +0530

    added test for Command Types

--- src/main/java/com/gojek/Model/Vehicle.java
+++ src/main/java/com/gojek/Model/Vehicle.java
@@ -9,22 +9,15 @@ public class Vehicle {
         this.color=color;
         this.registrationNumber=registrationNumber;
     }
+
     public String getColor() {
         return color;
     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
 
     public String getRegistrationNumber() {
         return registrationNumber;
     }
-
-    public void setRegistrationNumber(String registrationNumber) {
-        this.registrationNumber = registrationNumber;
-    }
-
 
 
 }
---
+++ src/test/java/com/gojek/Command/CommandTypesTest.java
@@ -0,0 +1,66 @@
+package com.gojek.Command;
+
+import com.gojek.Commands.CommandTypes;
+import com.gojek.Delegates.ParkingDelegateImpl;
+import com.gojek.Model.Vehicle;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CommandTypesTest {
+
+    @Mock
+    ParkingDelegateImpl parkingDelegate;
+
+    @Test
+    public void shouldCallParkVehicleMethodForPARKCommand() {
+
+        CommandTypes.PARK.takeAction(new String[]{"park", "MH-12-1234", "yellow"}, parkingDelegate);
+        verify(parkingDelegate).parkVehicle(Matchers.refEq(new Vehicle("yellow","MH-12-1234")) );
+    }
+
+    @Test
+    public void shouldCallCreateMethodForCREATECommand() {
+
+        CommandTypes.CREATE_PARKING_LOT.takeAction(new String[]{"create", "5"}, parkingDelegate);
+        verify(parkingDelegate).createParkingLot(5);
+    }
+
+    @Test
+    public void shouldCallLeaveMethodForLEAVECommand() {
+        CommandTypes.LEAVE.takeAction(new String[]{"leave", "5"}, parkingDelegate);
+        verify(parkingDelegate).leaveParkingSlot(5);
+    }
+
+    @Test
+    public void shouldCallStatusMethodForSTATUSCommand() {
+        CommandTypes.STATUS.takeAction(new String[]{"leave", "2"}, parkingDelegate);
+        verify(parkingDelegate).getStatusOfParkingLot();
+    }
+
+    @Test
+    public void shouldCallSlotNumberWithColor() {
+        CommandTypes.SLOT_NUMBERS_FOR_CARS_WITH_COLOUR.takeAction(new String[]{"status_with_color", "red"}, parkingDelegate);
+        verify(parkingDelegate).getSlotNumbersForCarsWithColor("red");
+    }
+
+    @Test
+    public void shouldCallRegistrationNumberWithColor() {
+        CommandTypes.REGISTRATION_NUMBERS_FOR_CARS_WITH_COLOUR.takeAction(new String[]{"registration_with_color", "red"}, parkingDelegate);
+        verify(parkingDelegate).getRegistrationNumbersForCarsWithColor("red");
+    }
+
+
+    @Test
+    public void shouldCallSlotNumberWithRegistrationNumber() {
+        CommandTypes.SLOT_NUMBER_FOR_REGISTRATION_NUMBER.takeAction(new String[]{"slot_number_for_registration", "MH 12 1234"}, parkingDelegate);
+        verify(parkingDelegate).getSlotNumberForRegistrationNumber("MH 12 1234");
+    }
+
+}
+
